ba68d9bd7a9f6534a3374c476f194a7dec687c43
EC2 Default User <ec2-user@ip-172-31-80-124.ec2.internal>
2019-06-18 02:02:40 +0000

    add status colum

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -44,8 +44,14 @@ class TasksController extends Controller
     public function store(Request $request)
     {
         //requestを保存するだけ
+        $this->validate($request, [
+            'content' => 'required|max:191',
+            'status' => 'required|max:10',
+        ]);
+        
         $task = new Task;
         $task->content = $request->content;
+        $task->status = $request->status;
         $task->save();
         return redirect('/');
     }
@@ -88,8 +94,14 @@ class TasksController extends Controller
      */
     public function update(Request $request, $id)
     {
+        $this->validate($request, [
+            'content' => 'required|max:191',
+            'status' => 'required|max:10',
+        ]);
+        
         $task = Task::find($id);
         $task->content = $request->content;
+        $task->status = $request->status;
         $task->save();
 
         return redirect('/');
